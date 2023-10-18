2e89c12a6fa192fb44e78b0480a510de47b36ad3
Mateus Vallim <mateus.vallim@usp.br>
2023-10-18 10:55:35 -0300

    feat: add mouses to homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -19,6 +19,14 @@ export default async function Home() {
       },
     },
   });
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
   return (
     <div className="flex flex-col gap-8">
       <PromoBanner
@@ -36,7 +44,7 @@ export default async function Home() {
         src="/banner-home-03.png"
         alt="Até 20% de desconto em fones"
       />
-      <ProductHorizontalList products={keyboards} title="Teclados" />
+      <ProductHorizontalList products={mouses} title="Mouses" />
     </div>
   );
 }
