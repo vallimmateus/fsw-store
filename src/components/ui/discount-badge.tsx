7fee1b6d3055e6e7b7aea34984b8564d1af13ee9
Mateus Vallim <mateus.vallim@usp.br>
2023-10-18 21:57:31 -0300

    fix: fix position of all DiscountBadge

--- src/components/ui/discount-badge.tsx
+++ src/components/ui/discount-badge.tsx
@@ -4,10 +4,7 @@ import { cn } from "@/lib/utils";
 
 const DiscountBadge = ({ children, className, ...props }: BadgeProps) => {
   return (
-    <Badge
-      className={cn("absolute left-3 top-3 px-2 py-0.5", className)}
-      {...props}
-    >
+    <Badge className={cn("px-2 py-0.5", className)} {...props}>
       <ArrowDownIcon size={14} />
       {children}%
     </Badge>
